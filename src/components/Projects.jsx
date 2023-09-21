80d9e9ee2432218322c5df55ae4f4793a35b963e
George Alvarez <georgealvarez@georges-mbp.mynetworksettings.com>
2023-09-21 00:01:27 -0400

    tr

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,49 +1,47 @@
 import React from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import FubarGaming from '../assets/fubarGaming.png';
 import Zelda from '../assets/Catacombs.png';
 import QRCode from '../assets/QRCode.png';
-import OUAT from '../assets/ouat1.png'
+import OUAT from '../assets/ouat1.png';
 
 const Projects = () => {
   return (
-    <div name='projects' className='w-full h-screen bg-[#0c1f3d] text-gray-300'>
-      <div className='max-w-[1000px] mx-auto p-4  flex flex-col justify-center  w-full h-full'>
+    <div name='projects' className='w-full min-h-screen bg-[#0c1f3d] text-gray-300'>
+      <div className='max-w-screen-sm mx-auto p-4'>
         <div>
-          <p className='text-4xl font-bold inline border-b-4 border-purple-600  text-gray-300'>Projects</p>
-          
+          <p className='text-2xl md:text-4xl font-bold inline border-b-4 border-purple-600 text-gray-300'>
+            Projects
+          </p>
         </div>
         <div className='max-w-xl mx-auto'>
           <Carousel infiniteLoop autoPlay interval={5000} showThumbs={false}>
-            <div className='shadow-md shadow-[#040c16]'>
-              <img className='w-20 mx-auto' src={FubarGaming} alt='project 1' />
+            <div className='shadow-md shadow-[#040c16] p-4 mb-4'>
+              <img className='w-full md:w-20 mx-auto' src={FubarGaming} alt='project 1' />
               <h3 className='my-4'>Fubar Gaming</h3>
-              <p> Fubar Gaming is a user-friendly game review website that empowers gamers to create and share their own reviews while providing ratings for their favorite games.</p>
+              <p>Fubar Gaming is a user-friendly game review website that empowers gamers to create and share their own reviews while providing ratings for their favorite games.</p>
             </div>
-            <div className='shadow-md shadow-[#040c16]'>
-              <img className='w-20 mx-auto' src={Zelda} alt='project 2' />
+            <div className='shadow-md shadow-[#040c16] p-4 mb-4'>
+              <img className='w-full md:w-20 mx-auto' src={Zelda} alt='project 2' />
               <h3 className='my-4'>Zelda Remake</h3>
-              <p> Zelda inspired remake using Kaboomjs game library.</p>
+              <p>Zelda inspired remake using Kaboomjs game library.</p>
             </div>
-            <div className='shadow-md shadow-[#040c16]'>
-              <img className='w-20 mx-auto' src={QRCode} alt='project 3' />
+            <div className='shadow-md shadow-[#040c16] p-4 mb-4'>
+              <img className='w-full md:w-20 mx-auto' src={QRCode} alt='project 3' />
               <h3 className='my-4'>QR code Generator</h3>
-              <p> Simple QR code generator.</p>
+              <p>Simple QR code generator.</p>
             </div>
-            <div className='shadow-md shadow-[#040c16]'>
-              <img className='w-20 mx-auto' src={OUAT} alt='project 4' />
+            <div className='shadow-md shadow-[#040c16] p-4 mb-4'>
+              <img className='w-full md:w-20 mx-auto' src={OUAT} alt='project 4' />
               <h3 className='my-4'>Once Upon A Time</h3>
-              <p> Top-down 2D role-playing game set in a vibrant fantasy world, where players embark on quests, battle foes, and explore an intricately designed landscape. (work in progess)
- 
- </p>
+              <p>Top-down 2D role-playing game set in a vibrant fantasy world, where players embark on quests, battle foes, and explore an intricately designed landscape. (work in progress)</p>
             </div>
-
           </Carousel>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Projects;
